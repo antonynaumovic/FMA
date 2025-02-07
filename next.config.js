e01e402d894ae259c060d878d957f2e1fb1f6e94
Antony Naumovic <40827853+antonynaumovic@users.noreply.github.com>
2025-02-07 23:31:39 +0000

    Test

--- next.config.js
+++ next.config.js
@@ -1,24 +1,19 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/FMA/' : '',
-  basePath: isProd ? '/FMA' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/FMA/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/FMA' : '',
   output: 'export'
 };
 
 
-// const nextConfig = {
-//     basePath: "/FMA",
-//     assetPrefix: '/FMA/',
-//     output: "export",  // <=== enables static exports
-//   };
-
 const withTM = require('next-transpile-modules')(['three']);
 
-module.exports = withTM(nextConfig);
+module.exports = nextConfig;
 
